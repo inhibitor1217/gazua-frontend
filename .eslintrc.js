6a0fdef67a2c978e1fb8672e0d5f7d515d95c6bd
Dongwook Hwang <cytops1217@gmail.com>
2018-10-24 19:55:37 +0900

    initial commit (just placeholder)

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "standard",
+    "rules": {
+        "indent": [
+            "warn",
+            4
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-unused-vars": 1
+    }
+};
