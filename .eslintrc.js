ee9165f4778de77ea9153a488f88f01b367ffb9b
Dongwook Hwang <cytops1217@gmail.com>
2018-10-24 21:38:51 +0900

    eject & applied scss modules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,13 @@ module.exports = {
     "rules": {
         "indent": [
             "warn",
-            4
+            2
         ],
         "semi": [
             "error",
             "always"
         ],
-        "no-unused-vars": 1
+        "no-unused-vars": 0,
+        "space-before-function-paren": 0
     }
 };
