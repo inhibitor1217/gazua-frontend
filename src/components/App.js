30ea3dcc5bbd6c5ea009f65c1b907430e1e8e350
Dongwook Hwang <cytops1217@gmail.com>
2018-10-26 02:20:02 +0900

    added scss-related modules and libraries

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Button } from 'components';
 
 class App extends Component {
     render() {
         return (
             <div>
+                <Button />
             </div>
         );
     }
---
+++ src/components/atoms/Button/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+const Button = () => {
+    return (
+        <div className="button">
+            Button
+        </div>
+    );
+};
+
+export default Button;
